672f7e40f1603a3d92ee6e9e2b93a30e11337fcc
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2024-02-10 18:34:35 +0700

    Add files via upload

---
+++ react_menu_remake/src/MenuDetail.js
@@ -0,0 +1,33 @@
+import { useNavigate, useParams } from "react-router-dom";
+import useFetch from "./UseFetch";
+
+const MenuDetail = () => {
+
+    const { id } = useParams()
+    const {data: menu, error} = useFetch("http://localhost:3030/menus/" + id)
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        fetch("http://localhost:3030/menus/" + menu.id, {
+            method: "DELETE"
+        }).then(() => {
+            navigate("/")
+        })
+    }
+
+    return ( 
+        <div className="menu-detail">
+            {error && <div>{ error }</div>}
+            {menu && (
+                <article>
+                    <h1>{menu.title}</h1>
+                    <h2>⭐{menu.rating}</h2>
+                    <p>{menu.content}</p>
+                    <button onClick={handleClick}>Delete</button>
+                </article>
+            )}
+        </div>
+     );
+}
+ 
+export default MenuDetail;
